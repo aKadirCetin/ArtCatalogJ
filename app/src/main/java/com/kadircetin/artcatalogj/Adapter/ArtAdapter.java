0041cd1aebc7d744914b0cbabc5ff069a6694bde
Kadir Çetin <kadir.cetin72@gmail.com>
2026-04-24 09:59:05 +0300

    ArtAdapter is added

---
+++ app/src/main/java/com/kadircetin/artcatalogj/Adapter/ArtAdapter.java
@@ -0,0 +1,63 @@
+package com.kadircetin.artcatalogj.Adapter;
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.kadircetin.artcatalogj.Model.Art;
+import com.kadircetin.artcatalogj.View.ArtActivity;
+import com.kadircetin.artcatalogj.databinding.RecyclerRowBinding;
+
+import java.util.ArrayList;
+
+public class ArtAdapter extends RecyclerView.Adapter<ArtAdapter.ArtHolder> {
+    ArrayList<Art> artArrayList;
+
+    public ArtAdapter(ArrayList<Art> artArrayList) {
+        this.artArrayList = artArrayList;
+    }
+
+    @NonNull
+    @Override
+    public ArtHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        RecyclerRowBinding recyclerRowBinding = RecyclerRowBinding.inflate(
+                LayoutInflater.from(parent.getContext()),
+                parent,
+                false);
+
+        return new ArtHolder(recyclerRowBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ArtHolder holder, int position) {
+        holder.binding.recyclerViewTextView.setText(artArrayList.get(holder.getAdapterPosition()).name);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(holder.itemView.getContext(), ArtActivity.class);
+                i.putExtra("info", "old");
+                i.putExtra("artId", artArrayList.get(holder.getAdapterPosition()).id);
+                holder.itemView.getContext().startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return artArrayList.size();
+    }
+
+    public class ArtHolder extends RecyclerView.ViewHolder {
+        private RecyclerRowBinding binding;
+
+        public ArtHolder(RecyclerRowBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+}
---
+++ app/src/main/java/com/kadircetin/artcatalogj/Model/Art.java
@@ -0,0 +1,10 @@
+package com.kadircetin.artcatalogj.Model;
+
+public class Art {
+    public String name;
+    public int id;
+    public Art(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
