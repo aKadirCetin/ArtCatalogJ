1213d48fbe6d6e6d0c4f4f2dae4379fa148b59d6
Kadir Çetin <kadir.cetin72@gmail.com>
2026-04-27 11:15:02 +0300

    MainActivity is added.

--- app/src/main/java/com/kadircetin/artcatalogj/View/MainActivity.java
+++ app/src/main/java/com/kadircetin/artcatalogj/View/MainActivity.java
@@ -1,23 +1,86 @@
 package com.kadircetin.artcatalogj.View;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.kadircetin.artcatalogj.Adapter.ArtAdapter;
+import com.kadircetin.artcatalogj.Model.Art;
 import com.kadircetin.artcatalogj.R;
+import com.kadircetin.artcatalogj.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<Art> artArrayList;
+    ArtAdapter artAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        artArrayList = new ArrayList<>();
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        artAdapter = new ArtAdapter(artArrayList);
+        binding.recyclerView.setAdapter(artAdapter);
+
+        getData();
         settings();
+    }
+    public void getData(){
+        try {
+            SQLiteDatabase sqLiteDatabase = this.openOrCreateDatabase("Arts",MODE_PRIVATE,null);
+            Cursor cursor=sqLiteDatabase.rawQuery("SELECT * FROM arts",null);
+            int nameIx = cursor.getColumnIndex("artName");
+            int idIx=cursor.getColumnIndex("id");
+
+            while(cursor.moveToNext()){
+                String name=cursor.getString(nameIx);
+                int id= cursor.getInt(idIx);
+
+                Art art = new Art(name,id);
+                artArrayList.add(art);
+            }
+            artAdapter.notifyDataSetChanged();
+            cursor.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.art_menu,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.add_art){
+            Intent intent= new Intent(MainActivity.this, ArtActivity.class);
+            intent.putExtra("info","new");
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void settings(){
